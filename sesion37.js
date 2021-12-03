b9258afed1e0f8379dce515454c64caea1fc3214
RossiAlamuca22 <rosalindaap22@gmail.com>
2021-12-03 09:32:47 -0500

    Unidad nueva

---
+++ sesion37.js
@@ -0,0 +1,46 @@
+/* let numerosImpares = [1,2,3,5,7];
+cantidad = numerosImpares.length;
+console.log("primera forma:")
+for (i=0; cantidad>1; i++){
+    console.log(numerosImpares[i])
+} */
+/*
+console.log("sugunda forma:")
+for(const valor of numerosImpares){
+    console.log(valor);
+} */
+
+//Camnbiando valores 
+/*
+listaDeNumeros=[];
+listaDeNumeros[0]=100;
+listaDeNumeros[1]=101;
+listaDeNumeros[2]=-10;
+listaDeNumeros[3]=20;
+listaDeNumeros[4]=-5;
+console.log(listaDeNumeros[2])
+*/
+
+/*const mixedArray = [3, "hola mundo", 10.5];
+cantidad = mixedArray.length;
+for(i = 0; cantidad>i; i++){
+    console.log(mixedArray[i]);
+}*/
+
+ let questions = [
+     ['¿Cual es la capital de Peru?', 'Lima'],
+ ['¿Cuantos colores tiene el arcoiris?', 7],
+ ['¿Cual es la moneda de Peru?', 'Sol'],
+ ];
+
+ console.log(questions [0][0]);
+ console.log(questions [0][1]);
+ console.log(questions [1][0]);
+
+ console.log(questions [1][1])
+ console.log(questions[2][0])
+ console.log(questions[2][1])
+     
+                 
+
+
